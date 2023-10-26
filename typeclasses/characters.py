aaa0e2ed9304eedb8600883484c2b8ae5b045e45
Calabe Davis <calkdavis@gmail.com>
2023-10-26 14:13:21 -0600

    Added files to control random dice rolls for interactions and a traithandler module to store and manipulate traits for objects. Added initial ability scores and traits for character and room classes.

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Characters
 
@@ -8,8 +9,10 @@ creation commands.
 
 """
 from evennia.objects.objects import DefaultCharacter
-
 from .objects import ObjectParent
+from evennia.utils import lazy_property
+from world.traits import TraitHandler
+from world.randomness import return_a_roll as roll
 
 
 class Character(ObjectParent, DefaultCharacter):
@@ -32,5 +35,51 @@ class Character(ObjectParent, DefaultCharacter):
     at_post_puppet - Echoes "AccountName has entered the game" to the room.
 
     """
+    # pull in handlers for traits, ability, skills
+    @lazy_property
+    def traits(self):
+        """TraitHandler that manages character traits."""
+        return TraitHandler(self)
+    
+    @lazy_property
+    def ability(self):
+        """Traithandler that manages ability scores and other attributes for the character"""
+        return TraitHandler(self, db_attribute='ability')
+    
+    # TODO: Add skills
+    
+    def at_object_creation(self):
+        "Called only at object creation and with update command."
+        # clear attributes to avoid any conflicts as we change the character class
+        self.ability.clear()
+        
+        # add in ability scores
+        self.ability.add(key='Dex', name='Dexterity', type='static', \
+            base=roll(False, 80, '2d20'), extra={'progression' : 0})
+        self.ability.add(key='Str', name='Strength', type='static', \
+            base=roll(False, 80, '2d20'), extra={'progression' : 0})
+        self.ability.add(key='Vit', name='Vitality', type='static', \
+            base=roll(False, 80, '2d20'), extra={'progression' : 0})
+        self.ability.add(key='Per', name='Perception', type='static', \
+            base=roll(False, 80, '2d20'), extra={'progression' : 0})
+        self.ability.add(key='Wil', name='Willpower', type='static', \
+            base=roll(False, 80, '2d20'), extra={'progression' : 0})
+        
+        # add in secondary abilities
+        self.ability.add(key='mass', name='Mass', type='static', \
+            base=roll(False, 75))
+        self.ability.add(key="enc", name="Encumberance", type='counter', \
+            base=0, max=(self.ability.Str.current * .5))
+        self.ability.add(key='hp', name='Health', type='gauge', \
+            base=int((self.ability.Vit.current * .5) + (self.ability.Wil.current * .5)))
+        self.ability.add(key='sp', name='Stamina', type='gauge', \
+            base=int((self.ability.Str.current * .25) + (self.ability.Vit.current * .25)+ \
+            (self.ability.Wil.current * .25)))
+        self.ability.add(key='cp', name='Conviction', type='gauge', \
+            base=int((self.ability.Wil.current * .75) + (self.ability.Per.current * .25)))
 
-    pass
+        # add in storage for other needed info
+        self.db.info = {'Target': None, 'Mercy': True, 'Default Attack': 'unarmed_strike', \
+                        'In Combat': False, 'Position': 'standing', 'Sneaking' : False, \
+                        'Wimpy': 20, 'Yield': 50, 'Title': None}
+        self.db.wallet = {'GC': 0, 'SC': 0, 'CC': 100}
--- typeclasses/rooms.py
+++ typeclasses/rooms.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Room
 
@@ -6,8 +7,9 @@ Rooms are simple containers that has no location of their own.
 """
 
 from evennia.objects.objects import DefaultRoom
-
 from .objects import ObjectParent
+from evennia.utils import lazy_property
+from world.traits import TraitHandler
 
 
 class Room(ObjectParent, DefaultRoom):
@@ -21,4 +23,40 @@ class Room(ObjectParent, DefaultRoom):
     properties and methods available on all Objects.
     """
 
-    pass
+    # pull in handlers for traits, ability, skills
+    @lazy_property
+    def traits(self):
+        """TraitHandler that manages character traits."""
+        return TraitHandler(self)
+    
+    # TODO: Add biomes
+    
+    def at_object_creation(self):
+        "Called only at object creation and with update command."
+        # clear traits to avoid any conflicts as we change the room class
+        self.traits.clear()
+        
+        # set traits at room creation
+        self.traits.add(key="elevation", name="Elevation", type="static", \
+            base=1000) # elevation above sea level in meters
+        # ruggedness of terrain from 0.01 to 1. How hard is it to move across
+        # this room? combination of slope and terrain obstacles
+        self.traits.add(key="rot", name="Ruggedness of Terrain", \
+                        type="static", base=0.05)
+        # size of room, rough measure in square meters, default is large outdoor
+        # room 100m X 100m
+        self.traits.add(key="size", name="Room Size", \
+                        type="static", base=10000)
+        # maximum number of tracks to store. Some terrain allows for tracks to
+        # be readable even with a large amount of foot traffic.
+        self.traits.add(key="trackmax", name="Maximum Readable Tracks", \
+                        type="static", base=(round(self.traits.size.actual / 500))) # default of 20
+        # add encumberance to room to limit the amount of junk that can be
+        # dropped in a given room
+        self.traits.add(key="enc", name="Encumberance", type='counter', \
+                        base=0, max=(self.traits.size.actual * 10))
+        # Info traits of the room. Zone and environment type should be None or a stored object
+        self.db.info = {'Non-Combat Room': False, 'Outdoor Room': True, \
+                        'Zone': None, 'Environment Type': None}
+        # empty db attribute dictionary for storing track objects
+        self.db.tracks = {}
